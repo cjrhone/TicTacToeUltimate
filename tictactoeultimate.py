b8085cff3f6d15830c1e93ecb6c6e69df1cd76fc
CJ Rhone <charlesrhonejr@gmail.com>
2020-01-28 12:52:20 -0800

    Add tictactoeultimate with printed board to console

---
+++ tictactoeultimate.py
@@ -0,0 +1,7 @@
+board = ['--', '--', '--',
+         '--', '--', '--',
+         '--', '--', '--']
+
+print(board[0] + " | " + board[1] + " | " + board[2])
+print(board[3] + " | " + board[4] + " | " + board[5])
+print(board[6] + " | " + board[7] + " | " + board[8])
