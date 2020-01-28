93eee40a203ec4640d45195b2645ba1da53296f3
CJ Rhone <charlesrhonejr@gmail.com>
2020-01-27 18:36:52 -0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+#
+# THIS IS AN OPTION FOR SETTING BOARD AND CHECKING WIN CONDITIONS
+# boardArray = [['a','b','c'], ['e','f','g'], ['x','y','z']]
+# test = boardArray[0][0]
+# test2 = boardArray[2][2]
+# test3 = boardArray[1][1]
+# print(test)
+# print(test2)
+# print(test3)
+
+#PLAYER CLASS HEADSTART
+# class Player:
+#     def __init__(self, pieceValue, playerArr = [],  numberOfWins = 0):
+#         self.pieceValue = pieceValue
+#         self.playerArray = playerArr
+#         self.numWins = numberOfWins
+#     def __str__(self):
+#         return str(self.pieceValue) + " " + str(self.playerArray)
+#     def moreMethods(self):
+#         pass
+
+
+#TESTING PLAYER CLASS AND OUTPUTS
+# player1 = Player(0)
+# player1.pieceValue = "Y"
+# player1.playerArray = ["test",1,2,3]
+# print(player1)
+# def placePiece(coordinateX, coordinateY, player):
+#     boardArray[coordinateX][coordinateY]
+
